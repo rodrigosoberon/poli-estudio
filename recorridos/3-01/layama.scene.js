851f3b21bb2fba9ff456344b35a7d45de4a5a863
rodrigosoberon <rodrigo.soberon.a@gmail.com>
2022-08-29 11:12:08 -0300

    recorridos nuevos

--- recorridos/3-01/layama.scene.js
+++ recorridos/3-01/layama.scene.js
@@ -3,24 +3,25 @@
 function getLayamaCameras()
 {
    var layamaCameras = new BABYLON.SmartArray(0);
-   layamaCameras.push({n: "CAM_010000", a: "_ICAM_001", p: new BABYLON.Vector3(2869.97, 140, 737.064), l: new BABYLON.Vector3(2868.97, 140, 737.036)});
-   layamaCameras.push({n: "CAM_010001", a: "CAM_002", p: new BABYLON.Vector3(2853.43, 140, 562.375), l: new BABYLON.Vector3(2852.87, 140, 561.546)});
-   layamaCameras.push({n: "CAM_010002", a: "CAM_003", p: new BABYLON.Vector3(3187.36, 140, 562.375), l: new BABYLON.Vector3(3187.26, 140, 561.38)});
-   layamaCameras.push({n: "CAM_010003", a: "CAM_004", p: new BABYLON.Vector3(3154.75, 140, 211.048), l: new BABYLON.Vector3(3154.43, 140, 210.101)});
-   layamaCameras.push({n: "CAM_010004", a: "CAM_005", p: new BABYLON.Vector3(3305, 140, 214.101), l: new BABYLON.Vector3(3306, 140, 214.095)});
-   layamaCameras.push({n: "CAM_010005", a: "CAM_006", p: new BABYLON.Vector3(3338.88, 140, 562.375), l: new BABYLON.Vector3(3338.9, 140, 561.375)});
-   layamaCameras.push({n: "CAM_010006", a: "CAM_007", p: new BABYLON.Vector3(3467.81, 140, 562.375), l: new BABYLON.Vector3(3467.83, 140, 561.375)});
-   layamaCameras.push({n: "CAM_010007", a: "CAM_008", p: new BABYLON.Vector3(3338.88, 140, 436.475), l: new BABYLON.Vector3(3339.87, 140, 436.503)});
-   layamaCameras.push({n: "CAM_010008", a: "CAM_009", p: new BABYLON.Vector3(3503.33, 140, 377.82), l: new BABYLON.Vector3(3503.93, 140, 377.021)});
-   layamaCameras.push({n: "CAM_010009", a: "CAM_010", p: new BABYLON.Vector3(3640.67, 140, 348.43), l: new BABYLON.Vector3(3641.08, 140, 347.516)});
-   layamaCameras.push({n: "CAM_010010", a: "CAM_012", p: new BABYLON.Vector3(2948.37, 140, 576.986), l: new BABYLON.Vector3(2948.58, 140, 576.007)});
+   layamaCameras.push({n: "CAM_010000", a: "_ICAM_001", p: new BABYLON.Vector3(2869.97, 140, 737.064), l: new BABYLON.Vector3(2869.49, 140, 736.184)});
+   layamaCameras.push({n: "CAM_010001", a: "CAM_002", p: new BABYLON.Vector3(2853.43, 140, 562.375), l: new BABYLON.Vector3(2854.43, 140, 562.405)});
+   layamaCameras.push({n: "CAM_010002", a: "CAM_003", p: new BABYLON.Vector3(3187.36, 140, 575.939), l: new BABYLON.Vector3(3187.34, 140, 574.94)});
+   layamaCameras.push({n: "CAM_010003", a: "CAM_004", p: new BABYLON.Vector3(3154.75, 140, 211.048), l: new BABYLON.Vector3(3155.75, 140, 211.112)});
+   layamaCameras.push({n: "CAM_010004", a: "CAM_005", p: new BABYLON.Vector3(3305, 140, 214.101), l: new BABYLON.Vector3(3305.68, 140, 213.368)});
+   layamaCameras.push({n: "CAM_010005", a: "CAM_006", p: new BABYLON.Vector3(3338.88, 140, 576.739), l: new BABYLON.Vector3(3338.9, 140, 575.739)});
+   layamaCameras.push({n: "CAM_010006", a: "CAM_007", p: new BABYLON.Vector3(3467.81, 140, 562.375), l: new BABYLON.Vector3(3466.81, 140, 562.356)});
+   layamaCameras.push({n: "CAM_010007", a: "CAM_008", p: new BABYLON.Vector3(3338.88, 140, 436.475), l: new BABYLON.Vector3(3339.8, 140, 436.098)});
+   layamaCameras.push({n: "CAM_010008", a: "CAM_009", p: new BABYLON.Vector3(3503.33, 140, 377.82), l: new BABYLON.Vector3(3504.32, 140, 377.68)});
+   layamaCameras.push({n: "CAM_010009", a: "CAM_010", p: new BABYLON.Vector3(3640.67, 140, 348.43), l: new BABYLON.Vector3(3640.69, 140, 349.43)});
+   layamaCameras.push({n: "CAM_010010", a: "CAM_012", p: new BABYLON.Vector3(2948.37, 140, 576.986), l: new BABYLON.Vector3(2948.75, 140, 576.059)});
+   layamaCameras.push({n: "CAM_010011", a: "CAM_015", p: new BABYLON.Vector3(3187.36, 140, 381.31), l: new BABYLON.Vector3(3186.74, 140, 382.094)});
    return layamaCameras;
 }
 
 function getLayamaResolutions()
 {
    var layamaResolutions = new BABYLON.SmartArray(0);
-   layamaResolutions.push("1024");
+   layamaResolutions.push("2048");
    layamaResolutions.push("1024");
    return layamaResolutions;
 }
--- recorridos/5-07/layama.scene.js
+++ recorridos/5-07/layama.scene.js
@@ -4,17 +4,17 @@ function getLayamaCameras()
 {
    var layamaCameras = new BABYLON.SmartArray(0);
    layamaCameras.push({n: "5-060004", a: "_ICAM_007", p: new BABYLON.Vector3(119.848, 140, 66.3153), l: new BABYLON.Vector3(120.714, 140, 66.8153)});
-   layamaCameras.push({n: "5-060000", a: "_CAM_002", p: new BABYLON.Vector3(474.261, 140, 555.416), l: new BABYLON.Vector3(475.258, 140, 555.329)});
-   layamaCameras.push({n: "5-060001", a: "_CAM_004", p: new BABYLON.Vector3(290.535, 140, 81.6096), l: new BABYLON.Vector3(291.354, 140, 82.1832)});
-   layamaCameras.push({n: "5-060002", a: "_CAM_005", p: new BABYLON.Vector3(473.093, 140, 365.511), l: new BABYLON.Vector3(473.352, 140, 366.477)});
-   layamaCameras.push({n: "5-060003", a: "_CAM_006", p: new BABYLON.Vector3(114.211, 140, 538.931), l: new BABYLON.Vector3(113.869, 140, 537.991)});
+   layamaCameras.push({n: "5-060000", a: "_CAM_002", p: new BABYLON.Vector3(474.261, 140, 555.416), l: new BABYLON.Vector3(475.168, 140, 554.994)});
+   layamaCameras.push({n: "5-060001", a: "_CAM_004", p: new BABYLON.Vector3(290.535, 140, 81.6096), l: new BABYLON.Vector3(290.535, 140, 82.6096)});
+   layamaCameras.push({n: "5-060002", a: "_CAM_005", p: new BABYLON.Vector3(473.093, 140, 365.511), l: new BABYLON.Vector3(473.959, 140, 366.011)});
+   layamaCameras.push({n: "5-060003", a: "_CAM_006", p: new BABYLON.Vector3(114.211, 140, 538.931), l: new BABYLON.Vector3(114.469, 140, 539.897)});
    return layamaCameras;
 }
 
 function getLayamaResolutions()
 {
    var layamaResolutions = new BABYLON.SmartArray(0);
-   layamaResolutions.push("1024");
+   layamaResolutions.push("2048");
    layamaResolutions.push("1024");
    return layamaResolutions;
 }
